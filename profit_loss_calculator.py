fac6a7a52480bf421a5c66b31d2a507645560ef2
0xNoob <captai988@gmail.com>
2026-01-13 19:05:22 -0500

    Create profit_loss_calculator.py

---
+++ profit_loss_calculator.py
@@ -0,0 +1,11 @@
+invested = float(input("How much did you invest? $"))
+current = float(input("What is it worth now? $"))
+
+difference = current - invented 
+
+if difference > 0:
+  print("Profit: $" + str(round(abs(difference, 2)))
+elif difference < 0:
+  print("Loss: $" + str(round(abs(difference), 2)))
+else:
+  print("Break even: $0.00")  
